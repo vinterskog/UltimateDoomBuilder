84555053fcfa706a6d776e749f0cd1a84b6202ca
biwa <6475593+biwa@users.noreply.github.com>
2020-08-07 00:40:30 +0200

    Add missing files for the docker

---
+++ Source/Plugins/UDBScript/Controls/ScriptDockerControl.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace CodeImp.DoomBuilder.UDBScript
+{
+	partial class ScriptDockerControl
+	{
+		/// <summary> 
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary> 
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Component Designer generated code
+
+		/// <summary> 
+		/// Required method for Designer support - do not modify 
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.filetree = new System.Windows.Forms.TreeView();
+			this.SuspendLayout();
+			// 
+			// filetree
+			// 
+			this.filetree.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.filetree.Location = new System.Drawing.Point(3, 3);
+			this.filetree.Name = "filetree";
+			this.filetree.Size = new System.Drawing.Size(296, 326);
+			this.filetree.TabIndex = 0;
+			this.filetree.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.filetree_AfterSelect);
+			// 
+			// ScriptDockerControl
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Controls.Add(this.filetree);
+			this.Name = "ScriptDockerControl";
+			this.Size = new System.Drawing.Size(302, 372);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TreeView filetree;
+	}
+}
---
+++ Source/Plugins/UDBScript/Controls/ScriptDockerControl.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CodeImp.DoomBuilder.UDBScript
+{
+	public partial class ScriptDockerControl : UserControl
+	{
+		public ScriptDockerControl(string foldername)
+		{
+			InitializeComponent();
+
+			FillTree(foldername);
+		}
+
+		private void FillTree(string foldername)
+		{
+			string path = Path.Combine(General.AppPath, foldername);
+			string[] files = Directory.GetFiles(path, "*", SearchOption.AllDirectories);
+
+			filetree.Nodes.AddRange(AddFiles(Path.Combine(path, "scripts")));
+			filetree.ExpandAll();
+		}
+
+		private TreeNode[] AddFiles(string path)
+		{
+			List<TreeNode> newnodes = new List<TreeNode>();
+
+			foreach (string directory in Directory.GetDirectories(path))
+			{
+				newnodes.Add(new TreeNode(Path.GetFileName(directory), AddFiles(directory)));
+			}
+
+			foreach (string filename in Directory.GetFiles(path))
+			{
+				if (Path.GetExtension(filename).ToLowerInvariant() == ".js")
+				{
+					TreeNode tn = new TreeNode(Path.GetFileNameWithoutExtension(filename));
+					tn.Tag = filename;
+
+					newnodes.Add(tn);
+				}
+			}
+
+			return newnodes.ToArray();
+		}
+
+		private void filetree_AfterSelect(object sender, TreeViewEventArgs e)
+		{
+			if (e.Node.Tag == null)
+				return;
+
+			if (e.Node.Tag is string)
+			{
+				BuilderPlug.Me.CurrentScriptFile = (string)e.Node.Tag;
+			}
+		}
+	}
+}
